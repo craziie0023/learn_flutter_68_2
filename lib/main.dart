8e814a4e709dd5eed9bb714455d7ff0c2029045c
craziie0023 <craziie0023@gmail.com>
2026-06-13 11:54:25 +0700

    Step 8: Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter_68_2/screen/home.dart';
+import 'package:learn_flutter_68_2/screen/item.dart';
 
 void main() {
   // runApp(const MyApp());
@@ -15,7 +16,7 @@ void main() {
           backgroundColor: Colors.pinkAccent,
           centerTitle: true,
         ), //AppBar
-        body: Home(),
+        body: Item(),
       ), // Scaffold
     ), // MaterialApp
   );
